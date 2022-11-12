62b1e83d07763241416129ec58e2a811bb659f82
Elia Zanotti <ELIA.ZANOTTI@local.ittcesena.it>
2022-11-12 09:15:06 +0100

    versione con la classe biblioteca

---
+++ biblioteca/biblioteca.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace biblioteca
+{
+    internal class biblioteca
+    {
+        private string nome;
+        private string indirizzo;
+        private string apertura;
+        private string chiusura;
+         List<string> libri;
+
+        public biblioteca(string nome, string indirizzo, string apertura, string chiusura, List<string> libri)
+        {
+            this.nome = nome;
+            this.indirizzo = indirizzo;
+            this.apertura = apertura;
+            this.chiusura = chiusura;
+            this.libri = libri;
+        }
+        public void addLibro(string nomeLibro)
+        {
+            libri.Add(nomeLibro); 
+        }
+
+        public int libriPresenti()
+        {
+            return libri.Count;
+        }
+    }
+}
